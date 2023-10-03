2e96a6df754a399fa04b25fadb4b4ee34c97c801
Théo Lansana <teolando.docs@gmail.com>
2023-10-03 00:33:43 -0400

    First deploy.

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap");
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: "Source Code Pro", monospace;
+}
+
+html {
+  background-color: #323232;
+}
+</style>
---
+++ src/components/WhatsAppButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <Button :link="whatsAppLink">
+    <slot></slot>
+  </Button>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+  props: {
+    tel: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    whatsAppLink() {
+      return `https://wa.me/${this.tel}`;
+    },
+  },
+  components: {
+    Button,
+  },
+};
+</script>
