970e22b09f94be35a726c3d6ee4a5d787570b886
Théo Lansana <teolando.docs@gmail.com>
2024-06-17 17:46:33 -0400

    Restart

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -6,7 +6,12 @@
 
 // Plugins
 import vuetify from './vuetify'
+import pinia from '@/stores'
+import router from '@/router'
 
 export function registerPlugins (app) {
-  app.use(vuetify)
+  app
+    .use(vuetify)
+    .use(router)
+    .use(pinia)
 }
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,11 @@
 // Plugins
+import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import Fonts from 'unplugin-fonts/vite'
+import Layouts from 'vite-plugin-vue-layouts'
 import Vue from '@vitejs/plugin-vue'
+import VueRouter from 'unplugin-vue-router/vite'
 import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-import ViteFonts from 'unplugin-fonts/vite'
 
 // Utilities
 import { defineConfig } from 'vite'
@@ -11,19 +14,36 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VueRouter(),
+    Layouts(),
     Vue({
       template: { transformAssetUrls }
     }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
-    Vuetify(),
+    Vuetify({
+      autoImport: true,
+      styles: {
+        configFile: 'src/styles/settings.scss',
+      },
+    }),
     Components(),
-    ViteFonts({
+    Fonts({
       google: {
         families: [{
           name: 'Roboto',
           styles: 'wght@100;300;400;500;700;900',
         }],
       },
+    }),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+      ],
+      eslintrc: {
+        enabled: true,
+      },
+      vueTemplate: true,
     }),
   ],
   define: { 'process.env': {} },
